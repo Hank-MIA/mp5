2b86466a7b09d5f8102e149454f6450b973f7863
Hao Zhang <hz97587@gmail.com>
2023-02-26 17:00:07 -0600

    part A first commit

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -4,27 +4,44 @@
 
 import sys
 from pyspark import SparkConf, SparkContext
+import re
 
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
+stopWords = set()
 with open(stopWordsPath) as f:
-	#TODO
+    line = f.readline().strip()
+    while line:
+        stopWords.add(line)
+        line = f.readline().strip()
 
+delimiters = []
+delimitersReg = ''
 with open(delimitersPath) as f:
-    #TODO
+    line = f.readline().strip()
+    while line:
+        delimiters.extend([*line])
+        line = f.readline().strip()
+for d in delimiters:
+    delimitersReg += '\\' + d + '|'
+delimitersReg = delimitersReg[:-1]
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
+words = lines.flatMap(lambda x: re.split(delimitersReg, x.strip().lower()))
+words = words.map(lambda x: x.strip())
+words = words.filter(lambda x: x != '' and x not in stopWords)
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x,y: x+y)
+topWordCounts = wordCounts.top(10, key=lambda x: (x[1], x[0]))
 
-#TODO
 
 outputFile = open(sys.argv[4],"w")
-
-#TODO
-#write results to output file. Foramt for each line: (line +"\n")
+for wc in topWordCounts.reverse():
+    outputFile.write('%s\t%s\n' % (wc[0], wc[1]))
+outputFile.close()
 
 sc.stop()
