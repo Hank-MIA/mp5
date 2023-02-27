9e90ae544e236d786c565546ca1eff83592d508a
Hao Zhang <hz97587@gmail.com>
2023-02-27 16:28:13 -0600

    part E first commit

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -10,15 +10,31 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
-
 leagueIds = sc.textFile(sys.argv[2], 1)
+leagueIds = leagueIds.map(lambda x: x.strip()).filter(lambda x: x != x.strip())
+league = sc.broadcast(set(leagueIds.collect()))
 
-#TODO
+
+def splitAndMap(l):
+    kvs = []
+    links = (l.split(':')[1]).split(' ')
+    for link in links:
+        if link != '' and link in league:
+            kvs.append((link, 1))
+    return kvs
+
+
+linkStream = lines.flatMap(lambda l: splitAndMap(l))
+linkCounts = linkStream.reduceByKey(lambda x,y: x+y)
+linkCountsList = linkCounts.collect()
+linkCountsList.sort()
 
 output = open(sys.argv[3], "w")
+for lc in linkCountsList:
+    output.write('%s\t%s' % (lc[0], lc[1]))
+output.close()
 
-#TODO
+
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
 
 sc.stop()
