276c0d555de8c49342397a56e00b9dee381a9922
Hao Zhang <hz97587@gmail.com>
2023-02-26 19:47:44 -0600

    part A 3rd commit

--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -8,18 +8,20 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1)
 
-#TODO
-
+lines = lines.map(lambda line: line.strip()).filter(lambda line: line != '')
+counts = lines.map(lambda line: int(line.split('\t')[1]))
+ans1 = counts.mean()
+ans2 = counts.sum()
+ans3 = counts.min()
+ans4 = counts.max()
+ans5 = counts.variance()
 outputFile = open(sys.argv[2], "w")
-'''
-TODO write your output here
-write results to output file. Format
 outputFile.write('Mean\t%s\n' % ans1)
 outputFile.write('Sum\t%s\n' % ans2)
 outputFile.write('Min\t%s\n' % ans3)
 outputFile.write('Max\t%s\n' % ans4)
 outputFile.write('Var\t%s\n' % ans5)
-'''
+outputFile.close()
 
 sc.stop()
 
